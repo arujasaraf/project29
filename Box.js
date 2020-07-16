3dfee80e24dd2a2413628f2fa651a1a70818659e
arujasaraf <65903153+arujasaraf@users.noreply.github.com>
2020-07-16 14:44:05 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+   // this.image = loadImage("sprites/wood1.png");
+  }
+
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill("blue");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
